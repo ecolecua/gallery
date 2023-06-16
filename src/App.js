4b57c2d0f37561ac20b7ad23b43af931a519fe46
Jose Gomez dev <josegomez.dev@gmail.com>
2023-06-15 20:46:06 -0600

    fix some warning and console errors

--- src/App.js
+++ src/App.js
@@ -41,52 +41,52 @@ function App() {
     <div className="App">
       <div id="loader-wrapper">
         <div id="loader"></div>
-        <div class="loader-section section-left"></div>
-        <div class="loader-section section-right"></div>
+        <div className="loader-section section-left"></div>
+        <div className="loader-section section-right"></div>
       </div>
-      <div class="tm-main-container">
-        <div class="tm-top-container">
-          <nav id="tmNav" class="tm-nav">
-            <a class="tm-navbar-menu" href="#">
+      <div className="tm-main-container">
+        <div className="tm-top-container">
+          <nav id="tmNav" className="tm-nav">
+            <a className="tm-navbar-menu" href="#">
               Menu
             </a>
-            <ul class="tm-nav-links">
-              <li class="tm-nav-item active">
+            <ul className="tm-nav-links">
+              <li className="tm-nav-item active">
                 <a
                   href="#"
                   data-linkid="0"
                   data-align="right"
-                  class="tm-nav-link"
+                  className="tm-nav-link"
                 >
                   Intro
                 </a>
               </li>
-              <li class="tm-nav-item">
+              <li className="tm-nav-item">
                 <a
                   href="#"
                   data-linkid="1"
                   data-align="right"
-                  class="tm-nav-link"
+                  className="tm-nav-link"
                 >
                   About
                 </a>
               </li>
-              <li class="tm-nav-item">
+              <li className="tm-nav-item">
                 <a
                   href="#"
                   data-linkid="2"
                   data-align="middle"
-                  class="tm-nav-link"
+                  className="tm-nav-link"
                 >
                   Work
                 </a>
               </li>
-              {/* <li class="tm-nav-item">
+              {/* <li className="tm-nav-item">
                 <a
                   href="#"
                   data-linkid="3"
                   data-align="left"
-                  class="tm-nav-link"
+                  className="tm-nav-link"
                 >
                   Contact
                 </a>
@@ -94,29 +94,29 @@ function App() {
             </ul>
           </nav>
 
-          <header class="tm-site-header-box tm-bg-dark">
-            <h1 class="tm-site-title">
+          <header className="tm-site-header-box tm-bg-dark">
+            <h1 className="tm-site-title">
               <img src={LogoLetter} alt="" /> <br />
               Tu Colección Fotográfica en un Solo Lugar
             </h1>
           </header>
         </div>
 
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-12">
-              <div class="tm-content">
-                <section class="tm-section tm-section-0">
-                  <h2 class="tm-section-title mb-3 font-weight-bold">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="tm-content">
+                <section className="tm-section tm-section-0">
+                  <h2 className="tm-section-title mb-3 font-weight-bold">
                     Galería Digital
                   </h2>
-                  <div class="tm-textbox tm-bg-dark">
+                  <div className="tm-textbox tm-bg-dark">
                     <h3>
                       <strong>
                         ¡Almacena y Comparte tus Recuerdos Fotográficos!
                       </strong>
                     </h3>
-                    <p class="mb-0">
+                    <p className="mb-0">
                       Nuestra aplicación revolucionaria te permite subir y
                       almacenar tus fotos en una base de datos segura y
                       confiable. Con nuestra hermosa galería digital, podrás
@@ -128,29 +128,28 @@ function App() {
                     href="#"
                     id="tm_about_link"
                     data-linkid="1"
-                    class="tm-link"
+                    className="tm-link"
                   >
                     Read More
                   </a>
                 </section>
 
-                <section class="tm-section tm-section-1">
-                  <div class="tm-textbox tm-textbox-2 tm-bg-dark">
-                    <h2 class="tm-text-blue mb-4">About Me</h2>
-                    <p class="mb-4">
+                <section className="tm-section tm-section-1">
+                  <div className="tm-textbox tm-textbox-2 tm-bg-dark">
+                    <h2 className="tm-text-blue mb-4">About Me</h2>
+                    <p className="mb-4">
                       <strong>José Alejandro Gómez Castro</strong>
                     </p>
-                    <p class="mb-4">
+                    <p className="mb-4">
                       Tecnólogo Creativo, Desarrollador Moderno / Ing. de
                       Javascript & Músico
                     </p>
-                    <p class="mb-0">
+                    <p className="mb-0">
                       - E-mail: <strong>josegomez.dev@gmail.com</strong> <br />
                       WhatsApp / Telegram: <strong>
                         +(506) 6240-2974
                       </strong>{' '}
                       <br />
-                      <hr />
                       Github: <strong>Repositorio Digital</strong> <br />
                       Medium: <strong>Blog (josegomezdev)</strong> <br />
                       LinkedIn: <strong>Perfil Profesional</strong> <br />
@@ -160,29 +159,29 @@ function App() {
                       href="#"
                       id="tm_work_link"
                       data-linkid="2"
-                      class="tm-link m-0"
+                      className="tm-link m-0"
                     >
                       Next
                     </a>
                   </div>
                 </section>
 
-                <section class="gallery-container tm-section tm-section-2 mx-auto">
+                <section className="gallery-container tm-section tm-section-2 mx-auto">
                   <UploadForm />
                   <br />
-                  <div class="grid gallery tm-gallery">
+                  <div className="grid gallery tm-gallery">
                     {removeDuplicates(data).map((url, idx) => {
                       return (
                         <figure
                           key={`key-${idx}`}
-                          class="effect-goliath tm-gallery-item"
+                          className="effect-goliath tm-gallery-item"
                         >
                           <img
                             width={200}
                             height={100}
                             src={url}
                             alt={`Img-${idx}`}
-                            class=""
+                            className=""
                           />
                           <figcaption>
                             <h2>
@@ -198,40 +197,43 @@ function App() {
                   </div>
                 </section>
 
-                <section class="tm-section tm-section-3 tm-section-left">
-                  <form action="" class="tm-contact-form" method="post">
-                    <div class="form-group mb-4">
+                <section className="tm-section tm-section-3 tm-section-left">
+                  <form action="" className="tm-contact-form" method="post">
+                    <div className="form-group mb-4">
                       <input
                         type="text"
                         id="contact_name"
                         name="contact_name"
-                        class="form-control"
+                        className="form-control"
                         placeholder="Name"
                         required
                       />
                     </div>
-                    <div class="form-group mb-4">
+                    <div className="form-group mb-4">
                       <input
                         type="email"
                         id="contact_email"
                         name="contact_email"
-                        class="form-control"
+                        className="form-control"
                         placeholder="Email"
                         required
                       />
                     </div>
-                    <div class="form-group mb-4">
+                    <div className="form-group mb-4">
                       <textarea
                         rows="4"
                         id="contact_message"
                         name="contact_message"
-                        class="form-control"
+                        className="form-control"
                         placeholder="Message"
                         required
                       ></textarea>
                     </div>
-                    <div class="form-group mb-0">
-                      <button type="submit" class="btn tm-send-btn tm-fl-right">
+                    <div className="form-group mb-0">
+                      <button
+                        type="submit"
+                        className="btn tm-send-btn tm-fl-right"
+                      >
                         Send
                       </button>
                     </div>
@@ -242,8 +244,8 @@ function App() {
           </div>
         </div>
 
-        <div class="tm-bottom-container">
-          <div class="tm-barcode-box">
+        <div className="tm-bottom-container">
+          <div className="tm-barcode-box">
             <img src={Logo} alt="Bar code" />
           </div>
 
